a474cc3479a1742159f2e124ee9e604d5bf96f0e
zakorgyula <gyula.zakor@gmail.com>
2016-08-02 14:13:15 +0200

    Add Arc to discovery session

--- src/fake_discovery_session.rs
+++ src/fake_discovery_session.rs
@@ -1,17 +1,18 @@
 use fake_adapter::FakeBluetoothAdapter;
 use std::error::Error;
+use std::sync::Arc; 
 
 #[derive(Debug)]
 pub struct FakeBluetoothDiscoverySession {
-    adapter: FakeBluetoothAdapter,
+    adapter: Arc<FakeBluetoothAdapter>,
 }
 
 impl FakeBluetoothDiscoverySession {
-    pub fn create_session(adapter: FakeBluetoothAdapter) -> Result<FakeBluetoothDiscoverySession, Box<Error>> {
+    pub fn create_session(adapter: Arc<FakeBluetoothAdapter>) -> Result<FakeBluetoothDiscoverySession, Box<Error>> {
         Ok(FakeBluetoothDiscoverySession::new(adapter))
     }
 
-    fn new(adapter: FakeBluetoothAdapter) -> FakeBluetoothDiscoverySession {
+    fn new(adapter: Arc<FakeBluetoothAdapter>) -> FakeBluetoothDiscoverySession {
         FakeBluetoothDiscoverySession {
             adapter: adapter,
         }
